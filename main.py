25717f0275c6391a8face9e97ae109b1971470a4
Yash Maurya <maurya.yash152@gmail.com>
2025-01-27 16:31:18 -0500

    working code with basic streamlit ui

--- main.py
+++ main.py
@@ -2,7 +2,16 @@ from langchain_ollama import OllamaEmbeddings
 from langchain_ollama.llms import OllamaLLM
 from langchain_core.vectorstores import InMemoryVectorStore
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.document_loaders import PDFPlumberLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+import streamlit as st
 
+template = """
+You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer just say that you don't know. Use three sentences maximum and keep the answer concise.
+Question: {question}
+Context: {context}
+Answer: 
+"""
 
 pdf_dir = "./pdfs/"
 
@@ -12,19 +21,53 @@ vector_store = InMemoryVectorStore(embeddings)
 model = OllamaLLM(model = "deepseek-r1:8b")
 
 def upload_pdf(file):
-    pass
+    with open(pdf_dir + file.name, "wb") as f:
+        f.write(file.getbuffer())
 
-def load_pdf(file):
-    pass
+def load_pdf(file_path):
+    loader = PDFPlumberLoader(file_path)
+    documents = loader.load()
+    return documents
 
 def split_text(documents):
-    pass
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 1000,
+        chunk_overlap = 200,
+        add_start_index = True,
+    )
+
+    return text_splitter.split_documents(documents)
 
 def index_docs(documents):
-    pass
+    vector_store.add_documents(documents)
 
 def retrieve_docs(query):
-    pass
+    return vector_store.similarity_search(query)
 
-def answer_question(question, context):
-    pass
+def answer_question(question, documents):
+    context = "\n\n".join([doc.page_content for doc in documents])
+    prompt = ChatPromptTemplate.from_template(template)
+    chain = prompt | model
+    return chain.invoke({"question": question, "context": context})
+
+
+uploaded_file = st.file_uploader(
+    "Upload PDF",
+    type="pdf",
+    accept_multiple_files=False,
+)
+
+if uploaded_file:
+    upload_pdf(uploaded_file)
+    documents = load_pdf(pdf_dir + uploaded_file.name)
+    chunked_documents = split_text(documents)
+    index_docs(chunked_documents)
+
+    question = st.chat_input("Ask a question")
+
+    if question:
+        st.chat_message("user").write(question)
+        related_documents = retrieve_docs(question)
+        ans = answer_question(question, related_documents)
+        st.chat_message("assistant").write(ans)
+
